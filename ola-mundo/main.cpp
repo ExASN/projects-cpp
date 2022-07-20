a34a15fc5ff0eb86db5ab9932618aaced7b5d087
ExASN <expedito.avelino@gmail.com>
2022-07-20 17:04:17 -0300

    ola-mundo

---
+++ ola-mundo/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+std::string nome;
+int x;
+int main(){
+    std::cout<<"Digite seu nome:\n";
+    std::cin>>nome;
+    std::cout<<nome<<"\n";
+    std::cout<<"Digite sua idade:\n";
+    std::cin>>x;
+    std::cout<<x<<"\n";
+    return 0;
+}
