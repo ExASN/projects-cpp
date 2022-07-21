a98b7b7b5e7f76bc4a16646b1dfe12fd27a97aaa
ExASN <expedito.avelino@gmail.com>
2022-07-20 21:11:27 -0300

    Create aluno.h

---
+++ poo/aluno.h
@@ -0,0 +1,20 @@
+#ifndef ALUNO_H_INCLUDED
+#define ALUNO_H_INCLUDED
+class aluno
+{
+private:
+    /* data */
+public:
+    aluno(/* args */);
+    ~aluno();
+};
+
+aluno::aluno(/* args */)//método construtor
+{
+}
+
+aluno::~aluno()
+{
+}
+
+#endif
