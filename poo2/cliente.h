f2e3d5645235ecf13b03fb927fd29ec95a3bc657
ExASN <expedito.avelino@gmail.com>
2022-07-22 14:52:00 -0300

    Implementação de gets e sets

--- poo2/cliente.h
+++ poo2/cliente.h
@@ -16,7 +16,33 @@ private:
     //date nascimento;
     
 public:
+    void setNome(string nm);
+    void setSobreNome(string snm);
+    void setLogradouro(string log);
+    void setComplemento(string cmp);
+    void setCpf(int c);
+    void setRg(int r);
+    void setCep(int ce);
+    void setNresidencia(int n);
+    void setNagencia(int na);
+    void setNconta(int nc);
+    void setTipoConta(int tc);
+    void setPacote(int pct);
+    string getNome();
+    string getSobreNome();
+    string getLogradouro();
+    string getComplemento();
+    int getCPF();
+    int getRg();
+    int getCep();
+    int getNresidencia();
+    int getNagencia();
+    int getNconta();
+    int getTipoConta();
+    int getPacote();
+
     void Consulta();
+
     Cliente(int perfil, int tpConta, int idCliente);
     ~Cliente();
 };
@@ -45,12 +71,79 @@ Cliente::~Cliente()
 {
 }
 
+void Cliente::setNome(string nm){
+    firstName=nm;
+}
+void Cliente::setSobreNome(string snm){
+    lastName=snm;
+}
+void Cliente::setLogradouro(string log){
+    logradouro=log;
+}
+void Cliente::setComplemento(string cmp){
+    complemento=cmp;
+}
+void Cliente::setCpf(int c){
+    cpf=c;
+}
+void Cliente::setRg(int r){
+    rg=r;
+}
+void Cliente::setCep(int ce){
+    cep=ce;
+}
+void Cliente::setNresidencia(int n){
+    nResidencia=n;
+}
+void Cliente::setNagencia(int na){
+    nAgencia=na;
+}
+void Cliente::setNconta(int nc){
+    nConta=nc;
+}
+void Cliente::setTipoConta(int tc){
+    tipoConta=tc;
+}
+void Cliente::setPacote(int pct){
+    pacote=pct;
+}
+
+int Cliente::getNconta(){
+    return nConta;
+}
+int Cliente::getNagencia(){
+    return nAgencia;
+}
+string Cliente::getNome(){
+    return firstName;
+}
+string Cliente::getSobreNome(){
+    return lastName;
+}
+int Cliente::getCPF(){
+    return firstName;
+}
+int Cliente::getRg(){
+    return rg;
+}
+string Cliente::getLogradouro(){
+    return logradouro;
+}
+int Cliente::getNresidencia(){
+    return nResidencia;
+}
+string Cliente::getComplemento(){
+    return complemento;
+}
+int Cliente::getCep(){
+    return cep;
+}
 
 void Cliente::Consulta(){
     cout<<"Numero da Conta.......: "<<nConta<<endl;
     cout<<"Numero da Agencia.....: "<<nAgencia<<endl;
     cout<<"Nome do Cliente.......: "<<firstName<<endl;
-    cout<<"Sobrenome do Cliente..: "<<firstName<<endl;
+    cout<<"Sobrenome do Cliente..: "<<lastName<<endl;
     cout<<"Cad. de Pesso Fisica..: "<<cpf<<endl;
     cout<<"Registro Geral........: "<<rg<<endl;
     cout<<"Endereço do Cliente...: "<<logradouro<<endl;
--- poo2/main.cpp
+++ poo2/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
 
     Cliente *cl1=new Cliente(0,0,0);
-    cl1->Consulta();
+    
     return 0;
 }
