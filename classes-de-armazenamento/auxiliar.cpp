92ac1604ef49840594bf1c8aa26ddb48375e5df5
Expedito Avelino da Silva Neto <expedito.avelino@gmail.com>
2022-08-10 16:14:25 -0300

    auxiliar

---
+++ classes-de-armazenamento/auxiliar.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int num;
+void impNum(){
+    cout << num << endl;
+}
---
+++ classes-de-armazenamento/extern.cpp
@@ -0,0 +1,16 @@
+/*
+ - Classe de armazenamento (extern)
+ - Eng. Expedito Avelino da Silva Neto
+ - 10 de agosto de 2022, Fortaleza - Ce
+*/
+#include <iostream>
+
+using namespace std;
+
+auto num{10};
+void impNum();
+
+int main(){
+    impNum();
+    return 0;
+}
