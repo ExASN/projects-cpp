787c43e6e4066a1659c9563554489efec08c0f70
ExASN <expedito.avelino@gmail.com>
2023-01-12 20:20:41 -0300

    My first commit of studying

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
-#include <stack> //Biblioteca necessárria para trabalhar com pilhas
+#include <stack>
 
 using namespace std;
 
-int main(){
-    stack <string> carros;                      //Cria uma pilha que vai armazenar variáveis do tipo string. O nome da pilha é "carros".
-    cout << endl << carros.empty() << endl;     //Comando verifica e caso a pilha esteja vazia, retorna true ou 1.
-    carros.push("Ford - Ka");                   //O pomando push("") insere um elemento no topo da pilha.
-    carros.push("Ford - Ecosporte");
-    carros.push("Renault - Sandero");
-    carros.push("Jeep - Compass");
-
-    cout << "O ultimo carro comprado foi : " 
-        << carros.top() << endl;                //O comando top() consulta o elemento no topo da pilha.
-
-    cout << "Quantidade de carros que temos: " 
-        <<carros.size() << endl;                //O comando size() informa o tamanho da pilha.
-
-    carros.pop();                               //O comando pop() descarta o elemento no topo da pilha.
-
-    cout << "O ultimo carro comprado foi : " 
-        << carros.top() << endl;
-
-    cout << "Quantidade de carros que temos: " 
-        <<carros.size() << endl;
-
+int main()
+{
     return 0;
 }
