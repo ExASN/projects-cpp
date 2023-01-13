d78bad8bf036ec148b94eb33fc26ae18be2c8680
ExASN <expedito.avelino@gmail.com>
2023-01-12 21:06:43 -0300

    uso de empty

--- stack/main.cpp
+++ stack/main.cpp
@@ -5,5 +5,34 @@ using namespace std;
 
 int main()
 {
+    stack <string> numbers;
+    numbers.push("zero");
+    numbers.push("one");
+    numbers.push("two");
+    numbers.push("three");
+    numbers.push("four");
+    numbers.push("five");
+    numbers.push("six");
+    numbers.push("seven");
+    numbers.push("eight");
+    numbers.push("nine");
+
+    // cout << "This is the size of stack: " << numbers.size() << endl;
+    // cout << "This is the number on the stack: " << numbers.top() << endl;
+
+    // numbers.pop();
+
+    // cout << "This is the size of stack: " << numbers.size() << endl;
+    // cout << "This is the number on the stack: " << numbers.top() << endl;
+
+    for(int x = numbers.size(); x>0;)
+    {
+    cout << "This is the size of stack: " << numbers.size() << endl;
+    cout << "This is the number on the stack: " << numbers.top() << endl;
+
+    numbers.pop();
+    }
+    cout << "This is the size of stack: " << numbers.size() << endl;
+
     return 0;
 }
